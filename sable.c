f28e655c45b81cd30a11ac34db10b875bab514de
Dysta <xav4098@gmail.com>
2020-04-10 15:23:21 +0200

    upgrade version vec

--- sable.c
+++ sable.c
@@ -5,7 +5,8 @@
 
 #if defined(ENABLE_VECTO) && (VEC_SIZE == 8)
 
-static void do_tile_vec(int x, int y, int width, int height);
+static unsigned do_tile_vec(int x, int y, int width, int height);
+static long unsigned int *buffer = NULL;
 
 #else
 
@@ -14,6 +15,7 @@ static void do_tile_vec(int x, int y, int width, int height);
 #endif
 
 #define do_tile_vec(x, y, w, h) do_tile((x), (y), (w), (h), (0))
+#define compute_new_state_vec(i, j) compute_new_state(i, j)
 
 #endif
 
@@ -23,16 +25,29 @@ static unsigned long int max_grains;
 
 #define table(i, j) TABLE[(i)*DIM + (j)]
 
+static inline long unsigned int* table_addr(int i, int j) {
+    return &(TABLE[i * DIM + j]);
+}
+
 #define RGB(r, v, b) (((r) << 24 | (v) << 16 | (b) << 8) | 255)
 
 void sable_init()
 {
-    TABLE = calloc(DIM * DIM, sizeof(long unsigned int));
+    // TABLE = calloc(DIM * DIM, sizeof(long unsigned int));
+    TABLE = aligned_alloc(32, DIM * DIM * sizeof(long unsigned int));
+
+    #if defined(ENABLE_VECTO) && (VEC_SIZE == 8)
+    buffer = _mm_malloc(DIM * DIM * sizeof(long unsigned int), 32);
+    #endif
 }
 
 void sable_finalize()
 {
     free(TABLE);
+
+    #if defined(ENABLE_VECTO) && (VEC_SIZE == 8)
+    _mm_free(buffer);
+    #endif
 }
 
 ///////////////////////////// Production d'une image
@@ -213,7 +228,7 @@ unsigned sable_compute_ompfor_n(unsigned nb_iter)
     return 0;
 }
 
-unsigned sable_compute_ompfor_pair_impaire(unsigned nb_iter)
+unsigned sable_compute_ompfor_pair_impair(unsigned nb_iter)
 {
     for (unsigned it = 1; it <= nb_iter; it++)
     {
@@ -222,8 +237,8 @@ unsigned sable_compute_ompfor_pair_impaire(unsigned nb_iter)
         #pragma omp parallel for reduction(+:changes)
         for (int y = 0; y < DIM; y+=TILE_SIZE)
         {
-           if (y % 2 == 0)
-            changes += do_tile(1, y + (y == 0),
+            if (y % 2 == 0)
+                changes += do_tile(1, y + (y == 0),
                             DIM - 2 ,
                             TILE_SIZE - ((y + TILE_SIZE == DIM) + (y == 0)),
                             omp_get_thread_num());
@@ -257,7 +272,7 @@ unsigned sable_compute_vec(unsigned nb_iter)
     for (unsigned it = 1; it <= nb_iter; it++)
     {
         // On traite toute l'image en un coup (oui, c'est une grosse tuile)
-        do_tile_vec(1, 1, DIM - 2, DIM - 2);
+        changement += do_tile_vec(1, 1, DIM - 2, DIM - 2);
         if (changement == 0)
             return it;
         changement = 0;
@@ -268,34 +283,26 @@ unsigned sable_compute_vec(unsigned nb_iter)
 #if defined(ENABLE_VECTO) && (VEC_SIZE == 8)
 #include <immintrin.h>
 
-static void compute_new_state_vec(int y, int x)
+static unsigned compute_new_state_vec(int y, int x)
 {
+    PRINT_DEBUG('c', "compute new state en [y x]-[%d %d]\n", y, x);
     /*
-    if (table(y x) >= 4) {
-        unsigned long int div4;
-        div4 = table(y, x) / 4;
-        table(y, x - 1) = div4;
-        table(y, x + 1) = div4;
-        table(y - 1, x) = div4;
-        table(x + 1, x) = div4;
-        table(y , x) %= 4;
-        chagement = 1;
+    if (table(y, x) >= 4)
+    {
+        unsigned long int div4 = table(y, x) / 4; -> table(y, x) >> 2
+        table(y, x - 1) += div4;
+        table(y, x + 1) += div4;
+        table(y - 1, x) += div4;
+        table(y + 1, x) += div4;
+        table(y, x) %= 4; -> &= 3
+        return 1;
     }
+    return 0;
     */
-
     // création d'un vecteur contenant table(y, x)
-    __m256i vecTable = _mm256_set_epi32(
-        table(y, x + 7),
-        table(y, x + 6),
-        table(y, x + 5),
-        table(y, x + 4),
-        table(y, x + 3),
-        table(y, x + 2),
-        table(y, x + 1),
-        table(y, x));
+    __m256i vecTable = _mm256_loadu_si256((const void*) table_addr(y, x));
     // création des vecteurs pour comparaisons et opérations
     __m256i vecThree = _mm256_set1_epi32(3);
-    __m256i vecZero = _mm256_set1_epi32(0);
 
     // compare vecTable[x] > 3, 0xFF si vrai, 0 sinon
     // reviens à comparer vecTable[x] >= 4
@@ -303,111 +310,51 @@ static void compute_new_state_vec(int y, int x)
 
     // Si il y a que des 0 dans le mask, on s'arrête la
     if (_mm256_testz_si256(mask, mask))
-        return;
+        return 0;
 
-    // création du vecteur contenant des 4 pour la division
-    __m256 vecFour = _mm256_set1_ps(4.0);
-
-    // on créer un vecteur qui contient table(y, x) si >= 4, 0 autrement
-    __m256 vecTableModif = _mm256_blendv_ps(
-        _mm256_cvtepi32_ps(vecZero),
-        _mm256_cvtepi32_ps(vecTable),
-        _mm256_cvtepi32_ps(mask));
-
-    // div4 = table(y, x) / 4 puis une conversion du resultat float -> int
-    // soit div4[x] contient table(y, x) / 4 si mask, 0 autrement (pour ne pas changer l'addition)
-    __m256i div4 = _mm256_cvtps_epi32(_mm256_div_ps(vecTableModif, vecFour));
+    // div4 = table(y, x) / 4
+    // soit div4[x] contient table(y, x) / 4, 0 autrement (pour ne pas changer l'addition)
+    __m256i div4 = _mm256_srli_epi32(vecTable, 2);
 
     // création des vecteurs contenant table(y - 1, x) et table(y + 1, x)
-    __m256i vecTable_XInf = _mm256_set_epi32(
-        table(y - 1, x + 7),
-        table(y - 1, x + 6),
-        table(y - 1, x + 5),
-        table(y - 1, x + 4),
-        table(y - 1, x + 3),
-        table(y - 1, x + 2),
-        table(y - 1, x + 1),
-        table(y - 1, x));
-    __m256i vecTable_XSup = _mm256_set_epi32(
-        table(y + 1, x + 7),
-        table(y + 1, x + 6),
-        table(y + 1, x + 5),
-        table(y + 1, x + 4),
-        table(y + 1, x + 3),
-        table(y + 1, x + 2),
-        table(y + 1, x + 1),
-        table(y + 1, x));
+    __m256i vecTable_XInf = _mm256_loadu_si256((const void*) table_addr(y-1,x));
+    __m256i vecTable_XSup = _mm256_loadu_si256((const void*) table_addr(y+1,x));
+    __m256i vecTable_YInf = _mm256_loadu_si256((const void*) table_addr(y,x-1));
+    __m256i vecTable_YSup = _mm256_loadu_si256((const void*) table_addr(y,x+1));
 
     // table(y - 1, x) += div4
     vecTable_XInf = _mm256_add_epi32(vecTable_XInf, div4);
     // table(y + 1, x) += div4
     vecTable_XSup = _mm256_add_epi32(vecTable_XSup, div4);
+    // table(y, x - 1) += div4
+    vecTable_YInf = _mm256_add_epi32(vecTable_YInf, div4);
+    // table(y, x + 1) += div4
+    vecTable_YSup = _mm256_add_epi32(vecTable_YSup, div4);
 
-    table(y - 1, x) = _mm256_extract_epi32(vecTable_XInf, 0);
-    table(y - 1, x + 1) = _mm256_extract_epi32(vecTable_XInf, 1);
-    table(y - 1, x + 2) = _mm256_extract_epi32(vecTable_XInf, 2);
-    table(y - 1, x + 3) = _mm256_extract_epi32(vecTable_XInf, 3);
-    table(y - 1, x + 4) = _mm256_extract_epi32(vecTable_XInf, 4);
-    table(y - 1, x + 5) = _mm256_extract_epi32(vecTable_XInf, 5);
-    table(y - 1, x + 6) = _mm256_extract_epi32(vecTable_XInf, 6);
-    table(y - 1, x + 7) = _mm256_extract_epi32(vecTable_XInf, 7);
+    _mm256_storeu_si256((__m256i*) table_addr(y-1, x), vecTable_XInf);
+    _mm256_storeu_si256((__m256i*) table_addr(y+1, x), vecTable_XSup);
+    _mm256_storeu_si256((__m256i*) table_addr(y, x-1), vecTable_YInf);
+    _mm256_storeu_si256((__m256i*) table_addr(y, x+1), vecTable_YSup);
 
-    table(y + 1, x) = _mm256_extract_epi32(vecTable_XSup, 0);
-    table(y + 1, x + 1) = _mm256_extract_epi32(vecTable_XSup, 1);
-    table(y + 1, x + 2) = _mm256_extract_epi32(vecTable_XSup, 2);
-    table(y + 1, x + 3) = _mm256_extract_epi32(vecTable_XSup, 3);
-    table(y + 1, x + 4) = _mm256_extract_epi32(vecTable_XSup, 4);
-    table(y + 1, x + 5) = _mm256_extract_epi32(vecTable_XSup, 5);
-    table(y + 1, x + 6) = _mm256_extract_epi32(vecTable_XSup, 6);
-    table(y + 1, x + 7) = _mm256_extract_epi32(vecTable_XSup, 7);
+    // table(y, x) %= 4 -> &= 3
+    __m256i modulus_data = _mm256_and_si256(vecTable, vecThree);
+    _mm256_storeu_si256((__m256i*) table_addr(y, x), modulus_data);
 
-    //FIXME: Passer en vectoriel cette partie
-    for (int i = 0; i < VEC_SIZE; i++)
-    {
-        if (table(y, x + i) >= 4)
-            table(y, x + i) = table(y, x) / 4;
-    }
-
-    //FIXME: Passer en vectoriel cette partie
-    for (int i = 0; i < VEC_SIZE; i++)
-    {
-        if (table(y, x - i) >= 4)
-            table(y, x - i) = table(y, x) / 4;
-    }
-
-    // FIXME: voir pour du vectoriel
-    if (table(y, x) >= 4)
-        table(y, x) %= 4;
-    if (table(y, x + 1) >= 4)
-        table(y, x + 1) %= 4;
-    if (table(y, x + 2) >= 4)
-        table(y, x + 2) %= 4;
-    if (table(y, x + 3) >= 4)
-        table(y, x + 3) %= 4;
-    if (table(y, x + 4) >= 4)
-        table(y, x + 4) %= 4;
-    if (table(y, x + 5) >= 4)
-        table(y, x + 5) %= 4;
-    if (table(y, x + 6) >= 4)
-        table(y, x + 6) %= 4;
-    if (table(y, x + 7) >= 4)
-        table(y, x + 7) %= 4;
-
-    // changement = 1;
+    return 1;
 }
 
-static void do_tile_vec(int x, int y, int width, int height)
+static unsigned do_tile_vec(int x, int y, int width, int height)
 {
 
     PRINT_DEBUG('c', "tuile [%d-%d][%d-%d] traitée\n", x, x + width - 1, y,
                 y + height - 1);
 
-    for (int i = y; i < y + height; i++)
+    unsigned changement = 0;
+    for (int i = y; i < y + height; i++) {
         for (int j = x; j < x + width; j += VEC_SIZE)
-        {
-            compute_new_state_vec(i, j);
-            // compute_multiple_pixels (i, j);
-        }
+            changement += compute_new_state_vec(i, j);
+    }
+    return changement;
 }
 
 #endif
